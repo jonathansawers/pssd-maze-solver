2587e313f794885d4864c81998b48a3c231f2e5c
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-10-03 23:15:48 +0930

    Added in start of maze solver with some setup + most DFS implementation

--- MazeSolver.cpp
+++ MazeSolver.cpp
@@ -1,1 +1,7 @@
 #include "MazeSolver.h"
+
+MazeSolver::MazeSolver() {}
+
+MazeSolver::MazeSolver(Maze &initMaze) { curMaze = &initMaze; }
+
+void MazeSolver::SetMaze(Maze &initMaze) { curMaze = &initMaze; }
