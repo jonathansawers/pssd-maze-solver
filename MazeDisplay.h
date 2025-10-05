820808ac8917ca96103fae5ab9db5c600b6f7d42
Jonathan <jonathansawers@icloud.com>
2025-10-05 17:17:32 +1030

    Maze display

--- MazeDisplay.h
+++ MazeDisplay.h
@@ -1,4 +1,29 @@
-//header file of mazedisplay class
-//containing functions and variables to display the maze
+#ifndef MAZEDISPLAY_H
+#define MAZEDISPLAY_H
 
 #include "Maze.h"
+#include <iostream>
+
+class MazeDisplay {
+    private:
+        const Maze* maze;
+        
+    public:
+        // Constructor
+        MazeDisplay(const Maze* m);
+        
+        // Display methods
+        void displayMaze() const;
+        void displayMazeWithPath(const std::vector<std::pair<int, int>>& path) const;
+        void displayMazeWithVisited(const std::vector<std::pair<int, int>>& visited) const;
+        
+        // Helper methods
+        void printHorizontalWall(int row) const;
+        void printCellRow(int row) const;
+        char getCellChar(int x, int y) const;
+        
+        // Display with different symbols
+        void displayMazeWithSymbols(const std::vector<std::pair<int, int>>& path, const std::vector<std::pair<int, int>>& visited) const;
+};
+
+#endif
