8d1a3d5f4f1418f1c148fc8aa080aef183208d09
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2025-10-06 17:34:56 +1030

    implemented working dfs + solution tracking options + display changes (now shows arrows for path)

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
+#include <iostream>
+
 #include "MazeDisplay.h"
 #include "MazeGenerator.h"
 #include "MazeSolver.h"
-#include <iostream>
 
 int main() {
-    MazeGenerator generator;
+  MazeGenerator generator;
 
-    //general check whether it generates correctly, delete when visual display is implemented
-    std::cout << "DFS Maze\n";
-    Maze dfsMaze = generator.generateDFS(20, 20); 
-    
-    MazeDisplay display(&dfsMaze);
-    display.displayMaze();
+  // general check whether it generates correctly, delete when visual display is
+  // implemented
+  std::cout << "DFS Maze\n";
+  Maze dfsMaze = generator.generateDFS(20, 20);
 
-    std::cout << "\nPrim's Maze \n";
-    Maze primMaze = generator.generatePrim(20, 20);//looks a bit weird
-    MazeDisplay display2(&primMaze);
-    display2.displayMaze();
+  MazeDisplay display(&dfsMaze);
+  display.displayMaze();
 
-    
-    return 0;
+  MazeSolver solver(dfsMaze);
+  solver.showFullNav(false);
+
+  auto solved = solver.solveMazeBlind(DFS);
+  display.displayMazeWithSymbols(solved.tracedPath, {});
+
+  solver.showFullNav(true);
+
+  solved = solver.solveMazeBlind(DFS);
+  display.displayMazeWithSymbols(solved.tracedPath, {});
+
+  //   std::cout << "\nPrim's Maze \n";
+  //   Maze primMaze = generator.generatePrim(20, 20);  // looks a bit weird
+  //   MazeDisplay display2(&primMaze);
+  //   display2.displayMaze();
+
+  return 0;
 }
