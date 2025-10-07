b14f985c31ed3957e35465c7725f2e3d67859ac7
Jonathan <jonathansawers@icloud.com>
2025-10-07 15:14:40 +1030

    Menu system class header complete

---
+++ MenuSystem.h
@@ -0,0 +1,47 @@
+#ifndef MENUSYSTEM_H
+#define MENUSYSTEM_H
+
+#include "Maze.h"
+#include "MazeGenerator.h"
+#include "MazeSolver.h"
+#include "MazeDisplay.h"
+#include <iostream>
+#include <string>
+
+class MenuSystem {
+private:
+    MazeGenerator generator;
+    Maze* currentMaze;
+    MazeDisplay* display;
+    MazeSolver* solver;
+    
+    // Menu display methods
+    void displayMainMenu() const;
+    void displayGenerationMenu() const;
+    void displaySolverMenu() const;
+    void displayDisplayMenu() const;
+    
+    // Input validation methods
+    int getValidInt(const std::string& prompt, int min = 1, int max = 100) const;
+    bool getValidBool(const std::string& prompt) const;
+    
+    // Menu action methods
+    void generateMaze();
+    void solveMaze();
+    void displayMaze();
+    void setMazeDimensions();
+    void setStartEndPositions();
+    
+    // Utility methods
+    void clearScreen() const;
+    void pauseForUser() const;
+    void showMazeInfo();
+    
+public:
+    MenuSystem();
+    ~MenuSystem();
+    
+    void run();
+};
+
+#endif
